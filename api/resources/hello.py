a5ae4676c63f9853f6a923f100a0d2631a66c670
Rafael Oliveira <rafael.oliveira@nutrien.com>
2022-04-26 19:50:08 -0300

    restarting repo

--- api/resources/hello.py
+++
@@ -1,7 +0,0 @@
-from flask_restful import Resource, request
-from cellphoneauth import auth_required
-
-class HelloResource(Resource):
-    # @auth_required
-    def get(self):
-        return {"message": "Hello from controle celular!"}, 200
