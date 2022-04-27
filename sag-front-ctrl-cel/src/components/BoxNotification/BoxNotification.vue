63bf731d115401833c59f2d60ce4ecb190439698
Rafael Oliveira <rafael.oliveira@nutrien.com>
2022-04-27 11:46:12 -0300

    Criacao componente BoxNotification

---
+++ sag-front-ctrl-cel/src/components/BoxNotification/BoxNotification.vue
@@ -0,0 +1,19 @@
+
+<template>
+    <div class="notification is-info">
+        {{ message }}
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+    name: 'BoxNotificationComponent',
+    props: {
+        message: {
+            type: String,
+            required: true,
+        }
+    }
+})
+</script>
