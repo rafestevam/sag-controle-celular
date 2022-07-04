91a983f7e56dacb74146625f6f4d44e702fd4c60
Rafael Oliveira <rafael.oliveira@nutrien.com>
2022-07-04 16:37:36 -0300

    Configs dos Modulos da App Vue

--- sag-front-ctrl-cel/src/store/index.ts
+++ sag-front-ctrl-cel/src/store/index.ts
@@ -1,15 +1,18 @@
-import INotification from "@/interfaces/INotification";
+import INotification, { IModalNotification } from "@/interfaces/INotification";
 import { InjectionKey } from "vue";
 import { createStore, Store, useStore as vuexUseStore } from "vuex";
 import { aparelho, AparelhoState } from "./modules/aparelho";
 import { centrocusto, CentroCustoState } from "./modules/centrocusto";
-import { NOTIFY } from "./modules/notif/constants/mutation-type";
+import { linha, LinhaState } from "./modules/linha";
+import { DELETE_NOTIFICATION, NOTIFY } from "./modules/notif/constants/mutation-type";
 
 export interface AppState {
     centrocusto: CentroCustoState,
     aparelho: AparelhoState,
+    linha: LinhaState,
     // notif: NotificationState,
     notifications: INotification[],
+    modalNotifications: IModalNotification[],
 }
 
 export const key: InjectionKey<Store<AppState>> = Symbol();
@@ -22,7 +25,11 @@ export const store = createStore<AppState>({
         aparelho: {
             aparelhos: [],
         },
+        linha: {
+            linhas: [],
+        },
         notifications: [],
+        modalNotifications: [],
     },
     mutations: {
         [NOTIFY](state, newNotification: INotification){
@@ -32,11 +39,21 @@ export const store = createStore<AppState>({
                 state.notifications = state.notifications
                     .filter(notif => notif.id != newNotification.id);
             }, 3000);
-        }
+        },
+        [DELETE_NOTIFICATION](state, idNotification: string){
+            state.notifications = state.notifications.filter(
+                notif => notif.id != idNotification
+            );
+        },
+        // [MODAL_NOTIFY](state, modalNotification: IModalNotification) {
+        //     // console.log(modalNotification);
+        //     state.modalNotifications.push(modalNotification);
+        // },
     },
     modules: {
         centrocusto,
         aparelho,
+        linha,
     },
 });
 
