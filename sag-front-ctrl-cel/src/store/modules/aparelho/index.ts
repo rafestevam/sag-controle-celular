dc82d925a55ce908bc76dd2e85a854680a462617
Rafael Oliveira <rafael.oliveira@nutrien.com>
2022-07-04 16:35:57 -0300

    Controle CRUD Modulo Aparelho

--- sag-front-ctrl-cel/src/store/modules/aparelho/index.ts
+++ sag-front-ctrl-cel/src/store/modules/aparelho/index.ts
@@ -1,31 +1,58 @@
 import httpClient from "@/http";
 import IAparelho from "@/interfaces/IAparelho";
-import { AppState } from "@/store";
+import { AppState, store } from "@/store";
 import { Module } from "vuex";
-import { GET_ALL_APARELHOS, POST_APARELHO } from "./constants/action-type";
+import {
+  DELETE_APARELHO,
+  GET_ALL_APARELHOS,
+  POST_APARELHO,
+  PUT_APARELHO,
+} from "./constants/action-type";
 import { LIST_ALL_APARELHOS } from "./constants/mutation-type";
 
 export interface AparelhoState {
-    aparelhos: IAparelho[],
+  aparelhos: IAparelho[];
 }
 
 export const aparelho: Module<AparelhoState, AppState> = {
-    mutations: {
-        [LIST_ALL_APARELHOS](state, aparelhos: IAparelho[]){
-            state.aparelhos = aparelhos;
-        }
+  mutations: {
+    [LIST_ALL_APARELHOS](state, aparelhos: IAparelho[]) {
+      state.aparelhos = aparelhos;
     },
-    actions: {
-        [GET_ALL_APARELHOS]({ commit }){
-            httpClient.get("aparelhos")
-                .then(resp => commit(LIST_ALL_APARELHOS, resp.data));
+  },
+  actions: {
+    [GET_ALL_APARELHOS]({ commit }) {
+      httpClient
+        .get("aparelhos")
+        .then((resp) => commit(LIST_ALL_APARELHOS, resp.data));
+    },
+    [POST_APARELHO](ctx, aparelho: IAparelho) {
+      return httpClient.post("aparelhos", aparelho, {
+        headers: {
+          "Content-Type": "application/json",
         },
-        [POST_APARELHO](ctx, aparelho: IAparelho) {
-            return httpClient.post("aparelhos", aparelho, {
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            });
-        }
-    }
-}
+      });
+    },
+    [PUT_APARELHO](ctx, aparelho: IAparelho) {
+      return httpClient
+        .put(`/aparelhos/${aparelho.id}`, aparelho, {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+        .then(() => {
+          const index = store.state.aparelho.aparelhos.findIndex(
+            (device) => device.id == aparelho.id
+          );
+          store.state.aparelho.aparelhos[index] = aparelho;
+        });
+    },
+    [DELETE_APARELHO](ctx, idAparelho: string) {
+      return httpClient.delete(`/aparelhos/${idAparelho}`).then(() => {
+        store.state.aparelho.aparelhos = store.state.aparelho.aparelhos.filter(
+          (device) => device.id != idAparelho
+        );
+      });
+    },
+  },
+};
