db953e6ee15181d7bd70cb12b473bce76702a863
Rafael Oliveira <rafael.oliveira@nutrien.com>
2022-07-04 16:28:29 -0300

    Modificações no modulo main da app vue

--- sag-front-ctrl-cel/src/main.ts
+++ sag-front-ctrl-cel/src/main.ts
@@ -2,10 +2,15 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import { key, store } from './store';
+import { msalPlugin } from './plugins/msalPlugin';
+import { msalInstance } from './authConfig';
 
 import '@fortawesome/fontawesome-free/css/all.css';
+import { maska } from 'maska';
 
 createApp(App)
     .use(router)
     .use(store, key)
+    .use(msalPlugin, msalInstance)
+    .directive('maska', maska)
     .mount('#app');
