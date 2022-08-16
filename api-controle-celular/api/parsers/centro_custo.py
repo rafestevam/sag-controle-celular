0b8e3fc60471bc37faa218378818001ba01679a5
Rafael Oliveira <rafael.oliveira@nutrien.com>
2022-08-16 17:36:03 -0300

    Ajuste de comentarios e nome de variaveis

--- api-controle-celular/api/parsers/centro_custo.py
+++ api-controle-celular/api/parsers/centro_custo.py
@@ -5,20 +5,22 @@ import sys
 class CentroCustoParser:
 
     @classmethod
-    def parseCSV(cls, filePath):
+    def parseCSV(cls, file_path):
         try:
             col_names = ['cc_cod', 'cc_nome']
-            csvData = pd.read_csv(filePath, names=col_names, header=None)
+            csvData = pd.read_csv(file_path, names=col_names, header=None)
 
-            #Iteração sobre o CSV para insersão no Banco de Dados
+            # Iteração sobre o CSV para insersão no Banco de Dados
             for i, row in csvData.iterrows():
                 if i == 0:
+                    # Validação das colunas esperadas
                     if(row['cc_cod'] != 'cc_cod' or row['cc_nome'] != 'cc_nome'):
                         raise RuntimeError("As colunas esperadas para o arquivo são 'cc_cod' e 'cc_nome'")
                 else:
                     cc_cod = str(row['cc_cod'])
                     cc_nome = str(row['cc_nome'])
                     
+                    # Verificação de duplicidade de registros
                     cc = CentroCustoModel.find_by_cc_cod(cc_cod)
                     if cc:
                         raise RuntimeError(f"Centro de Custo {cc_cod} já existente.")
