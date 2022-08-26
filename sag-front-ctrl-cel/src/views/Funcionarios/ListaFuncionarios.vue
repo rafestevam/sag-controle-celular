0d4f1096266f54465c659ee9d80baf0a815a96a6
Rafael Oliveira <rafael.oliveira@nutrien.com>
2022-08-26 20:37:29 -0300

    Alteracao do display da lista de funcionarios

--- sag-front-ctrl-cel/src/views/Funcionarios/ListaFuncionarios.vue
+++ sag-front-ctrl-cel/src/views/Funcionarios/ListaFuncionarios.vue
@@ -3,44 +3,37 @@
     <h1 class="subtitle">Lista de Funcionários</h1>
   </div>
   <section class="main-form">
+    <div class="sag-button">
+      <router-link to="/funcionarios/novo" class="button">
+        <span class="icon is-small">
+          <i class="fas fa-plus"></i>
+        </span>
+        <span>Adicionar</span>
+      </router-link>
+    </div>
     <div class="container is-widescreen">
       <table class="table is-fullwidth">
         <thead>
           <tr>
-            <td>CPF</td>
             <td>Nome do Crachá</td>
+            <td>CPF</td>
+            <td>Centro de Custo</td>
             <td>Cargo</td>
             <td>Ações</td>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>333.333.333-33</td>
-            <td>José das Couves</td>
-            <td>Consultor PS I</td>
+          <tr v-for="funcionario in funcionarios" :key="funcionario.id">
+            <td>{{ funcionario.nome_social }}</td>
+            <td>{{ funcionario.cpf}}</td>
+            <td>{{ funcionario.centro_custo?.cc_cod }}</td>
+            <td>{{ funcionario.cargo }}</td>
             <td>
-              <button class="button">
+              <router-link :to="`/funcionarios/${funcionario.id}`" class="button">
                 <span class="icon is-small">
                   <i class="fas fa-pencil-alt"></i>
                 </span>
-              </button>
-              <button class="button ml-2 is-danger">
-                <span class="icon is-small">
-                  <i class="fas fa-trash"></i>
-                </span>
-              </button>
-            </td>
-          </tr>
-          <tr>
-            <td>555.555.555-55</td>
-            <td>Antônio da Silva</td>
-            <td>Consultor TI</td>
-            <td>
-              <button class="button">
-                <span class="icon is-small">
-                  <i class="fas fa-pencil-alt"></i>
-                </span>
-              </button>
+              </router-link>
               <button class="button ml-2 is-danger">
                 <span class="icon is-small">
                   <i class="fas fa-trash"></i>
@@ -55,8 +48,18 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { useStore } from '@/store';
+import { GET_ALL_FUNC } from '@/store/modules/funcionario/constants/action-type';
+import { computed, defineComponent } from 'vue'
 export default defineComponent({
     name: 'ListaFuncionariosViewComponent',
+    setup() {
+      const store = useStore();
+      store.dispatch(GET_ALL_FUNC);
+      return {
+        store,
+        funcionarios: computed(() => store.state.funcionario.funcionarios),
+      }
+    },
 });
 </script>
