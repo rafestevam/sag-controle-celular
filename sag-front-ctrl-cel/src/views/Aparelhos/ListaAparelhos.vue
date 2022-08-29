134a7b131b06f64d5a7639d6fd7ca6ae2bed9ea4
Rafael Oliveira <rafael.oliveira@nutrien.com>
2022-08-29 11:37:47 -0300

    Implementacao da datatable de Aparelhos

--- sag-front-ctrl-cel/src/views/Aparelhos/ListaAparelhos.vue
+++ sag-front-ctrl-cel/src/views/Aparelhos/ListaAparelhos.vue
@@ -18,6 +18,48 @@
       </router-link>
     </div>
     <div class="container is-widescreen">
+
+      <!-- Numero de Entradas por pagina -->
+      <div class="field is-horizontal">
+        <div class="field-body">
+          <div class="field is-narrow">
+            <div class="control">
+              Mostrando
+            </div>
+          </div>
+          <div class="field is-narrow">
+            <div class="control">
+              <div class="select">
+                <select v-model="limitPerPage">
+                  <option :value="5">5</option>
+                  <option :value="10">10</option>
+                  <option :value="25">25</option>
+                  <option :value="50">50</option>
+                  <option :value="100">100</option>
+                </select>
+              </div>
+            </div>
+          </div>
+          <div class="field is-narrow">
+            <div class="control">
+              linhas por página
+            </div>
+          </div>
+        </div>
+
+        <!-- Input de busca de dados -->
+        <div class="field-body">
+          <div class="field is-grouped is-grouped-right">
+            <div class="control has-icons-right">
+              <input type="text" class="input" v-model="search" placeholder="Busca...">
+              <span class="icon is-small is-right">
+                <i class="fa-solid fa-magnifying-glass"></i>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <table class="table is-fullwidth" v-if="aparelhos">
         <thead>
           <tr>
@@ -30,7 +72,7 @@
           </tr>
         </thead>
         <tbody>
-          <tr v-for="aparelho in aparelhos" :key="aparelho.id">
+          <tr v-for="aparelho in filteredEntries" :key="aparelho.id">
             <td>{{ aparelho.numero_serie }}</td>
             <td>{{ aparelho.imei }}</td>
             <td>{{ aparelho.marca }}</td>
@@ -52,6 +94,22 @@
         </tbody>
       </table>
       <BoxNotification message="Não há aparelhos cadastrados na base de dados" v-else />
+      
+      <!-- Paginação da Tabela -->
+      <div class="pagination" role="pagination" aria-label="pagination">
+        <ul class="pagination-list">
+          <li><a href="#" @click.prevent="pageActive = 1" class="pagination-link">&#171;</a></li>
+          <li><a href="#" @click.prevent="pageActive = (pageActive > 1) ? pageActive - 1 : pageActive" class="pagination-link">&#8249;</a></li>
+
+          <li v-for="(page, index) in renderPagination" :key="index">
+            <a class="pagination-link" :class="{'is-current': pageActive === Number(page)}" href="#" @click.prevent="pageActive = Number(page)">{{ page }}</a>
+          </li>
+
+          <li><a href="#" @click.prevent="pageActive = (pageActive < totalPages) ? pageActive + 1 : pageActive" class="pagination-link">&#8250;</a></li>
+          <li><a href="#" @click.prevent="pageActive = totalPages" class="pagination-link">&#187;</a></li>
+        </ul>
+      </div>
+
     </div>
   </section>
 </template>
@@ -60,10 +118,11 @@
 import { useStore } from "@/store";
 import useNotificator from "@/hooks/Notificator";
 import { DELETE_APARELHO, GET_ALL_APARELHOS } from "@/store/modules/aparelho/constants/action-type";
-import { computed, defineComponent } from "vue";
+import { computed, defineComponent, ref } from "vue";
 import BoxNotification from "@/components/BoxNotification/BoxNotification.vue";
 import ModalNotification from "@/components/ModalNotification/ModalNotification.vue";
 import { NotificationType } from "@/interfaces/INotification";
+import { array as arr } from 'alga-js';
 
 export default defineComponent({
   name: "ListaAparelhosViewComponent",
@@ -84,10 +143,48 @@ export default defineComponent({
     const store = useStore();
     const { notify } = useNotificator();
     store.dispatch(GET_ALL_APARELHOS);
+
+    // Variaveis para Num de Entradas e Paginação da Tabela
+    const pageActive = ref<number>(1);
+    const limitPerPage = ref<number>(5);
+    const totalPages = ref<number>(1);
+    const search = ref<string>('');
+    const deletedAparelhoID = ref<string>('');
+
     return {
       aparelhos: computed(() => store.state.aparelho.aparelhos),
       store,
       notify,
+
+      limitPerPage,
+      pageActive,
+      totalPages,
+      search,
+      deletedAparelhoID,
+      // sort,
+      // Entradas filtradas de acordo com a paginação
+      filteredEntries: computed(() => {
+        let newEntries = store.state.aparelho.aparelhos;
+        if(search.value.length >= 2) {
+          newEntries = arr.search(store.state.aparelho.aparelhos, search.value);
+        }
+        //newEntries = arr.sort(newEntries, 'cc_cod', 'asc');
+        if(deletedAparelhoID.value != ""){
+          newEntries = newEntries.filter(
+            (dvc) => dvc.id != deletedAparelhoID.value
+          );
+        }
+        // eslint-disable-next-line vue/no-side-effects-in-computed-properties
+        totalPages.value = arr.pages(newEntries, limitPerPage.value);
+        newEntries = arr.paginate(newEntries, pageActive.value, limitPerPage.value);
+        return newEntries;
+      }),
+
+      // Renderização da paginação
+      renderPagination: computed(() => {
+        let pagination = arr.pagination(totalPages.value, pageActive.value, 2);
+        return pagination;
+      }),
     }
   },
   methods: {
@@ -99,6 +196,7 @@ export default defineComponent({
             NotificationType.SUCCESS,
             `Aparelho ${aparelhoSpec} excluído com sucesso!`
           );
+          this.deletedAparelhoID = id;
           this.modalActive = false;
         })
         .catch(err => {
