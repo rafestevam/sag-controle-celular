ac2a11fce993f89b79cba1d62d4ce6be3c8c0b30
Rafael Oliveira <rafael.oliveira@nutrien.com>
2022-08-31 19:15:06 -0300

    Retirando configs do MSAL

--- sag-front-ctrl-cel/src/App.vue
+++ sag-front-ctrl-cel/src/App.vue
@@ -1,60 +1,56 @@
 <template>
   <main class="is-gapless is-multiline">
     <header>
-      <MainNavigation/>
+      <MainNavigation />
     </header>
-    <Notification @onCloseNotification="closeNotification"/>
+    <Notification @onCloseNotification="closeNotification" />
     <router-view></router-view>
   </main>
 </template>
 
 <script lang="ts">
-import { defineComponent, onBeforeMount } from 'vue';
-import { loginRequest } from './authConfig';
-import MainNavigation from './components/MainNavigation.vue';
-import Notification from './components/Notifications/Notification.vue';
-import { useIsAuthenticated } from './hooks/msal/useIsAuthenticated';
-import { useMsal } from './hooks/msal/useMsal';
-import { useStore } from '@/store';
-import { DELETE_NOTIFICATION } from './store/modules/notif/constants/mutation-type';
+import { defineComponent, onBeforeMount } from "vue";
+import MainNavigation from "./components/MainNavigation.vue";
+import Notification from "./components/Notifications/Notification.vue";
+import { useStore } from "@/store";
+import { DELETE_NOTIFICATION } from "./store/modules/notif/constants/mutation-type";
 
 export default defineComponent({
-  name: 'App',
+  name: "App",
   components: {
     MainNavigation,
     Notification,
   },
   setup() {
     const store = useStore();
-    const authenticated = useIsAuthenticated();
-    const { instance } = useMsal();
+    // const authenticated = useIsAuthenticated();
+    // const { instance } = useMsal();
     onBeforeMount(() => {
-      if(!authenticated.value){
-        console.log(authenticated)
-        instance.loginRedirect(loginRequest)
-      }
+      // if(!authenticated.value){
+      //   console.log(authenticated)
+      //   instance.loginRedirect(loginRequest)
+      // }
     });
     return {
       store,
-    }
+    };
   },
   methods: {
     closeNotification(idNotif: string) {
-      this.store
-        .commit(DELETE_NOTIFICATION, idNotif);
-    }
-  }
+      this.store.commit(DELETE_NOTIFICATION, idNotif);
+    },
+  },
 });
 </script>
 
 <style>
-  .main-window {
-    padding: 1.25rem;
-  }
-  .main-form {
-    padding-top: 1.50rem;
-  }
-  .sag-button {
-    padding-bottom: 1rem;
-  }
+.main-window {
+  padding: 1.25rem;
+}
+.main-form {
+  padding-top: 1.5rem;
+}
+.sag-button {
+  padding-bottom: 1rem;
+}
 </style>
