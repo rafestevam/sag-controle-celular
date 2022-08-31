7682d76a8bf672f216833000de2d092a39c0c9ba
Rafael Oliveira <rafael.oliveira@nutrien.com>
2022-08-31 19:15:22 -0300

    Configuracao do vue-select

--- sag-front-ctrl-cel/src/main.ts
+++ sag-front-ctrl-cel/src/main.ts
@@ -4,6 +4,8 @@ import router from './router';
 import { key, store } from './store';
 import { msalPlugin } from './plugins/msalPlugin';
 import { msalInstance } from './authConfig';
+import vSelect from "vue-select";
+import "vue-select/dist/vue-select.css";
 
 import '@fortawesome/fontawesome-free/css/all.css';
 import { maska } from 'maska';
@@ -12,5 +14,6 @@ createApp(App)
     .use(router)
     .use(store, key)
     .use(msalPlugin, msalInstance)
+    .component('v-select', vSelect)
     .directive('maska', maska)
     .mount('#app');
