1472c924e1d9b984413a7712a6c04aa6fc41feeb
Rafael Oliveira <rafael.oliveira@nutrien.com>
2022-10-03 12:57:43 -0300

    Ajuste das configs do wsgi

--- api-controle-celular/api/wsgi.py
+++ api-controle-celular/api/wsgi.py
@@ -10,6 +10,8 @@ import sys
 import site
 
 site.addsitedir('/var/www/sag-controle-celular/controlcel/lib/python3.6/site-packages')
+sys.stdout = sys.stderr
 sys.path.insert(0, '/var/www/sag-controle-celular/api-controle-celular/api')
+
 
 from app import app as application
